a19e64834c026a1f3b56b8a6731962af9341d692
Dylan Li <dylan@Dylans-MacBook-Air.local>
2023-03-25 17:16:08 -0400

    Add components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
 	return (
 		<div>
-
+			{/* Menu at top */}
+			{/* Team */}
+			{/* Search for pokemon */}
 		</div>
   );
 }
---
+++ src/components/Dex.jsx
@@ -0,0 +1,11 @@
+
+function Dex() {
+	return (
+		<div>
+			
+		</div>
+	);
+
+}
+
+export default Dex;
