3a08b2b63d466c4ed23bd7793e4ae5ff8a1dc951
Dylan Li <dylan@Dylans-MacBook-Air.local>
2023-03-28 21:58:01 -0400

    add randomization feature and tooltips

--- src/components/Dex.jsx
+++ src/components/Dex.jsx
@@ -26,27 +26,68 @@ function Dex() {
 		.then(handleFetch)
 		.then(handleResponse)
 		.catch(handleError);
+		setOffset(10);
 		// eslint-disable-next-line
 	}, []);
 
 	const handleLoadMore = async () => {
-		const url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=' + (offset+10);
+		var url;
+		if (offset > 1271) {
+			url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=' + (offset);
+			fetch(url)
+			.then(handleFetch)
+			.then(handleResponse)
+			.catch(handleError);
+
+			url = `https://pokeapi.co/api/v2/pokemon?limit=${1281-offset}&offset=${0}`;
+			fetch(url)
+			.then(handleFetch)
+			.then(handleResponse)
+			.catch(handleError);
+
+			setOffset(1281-offset);
+		}
+		else {
+			url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=' + (offset);
+			fetch(url)
+			.then(handleFetch)
+			.then(handleResponse)
+			.catch(handleError);
+
+			setOffset(offset+10);
+		}
+		
+	}
+
+	const handleRandomize = (responseJSON) => {
+		const cards = responseJSON.results.map((item) => <Card key={item.name} name={item.name} url={item.url}/>);
+		setPokemonCards(cards);
+	};
+
+	const randomizeDex = () => {
+		const off = Math.floor(Math.random() * 1272);
+		const url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=' + off;
 		fetch(url)
 		.then(handleFetch)
-		.then(handleResponse)
+		.then(handleRandomize)
 		.catch(handleError);
 		
-		setOffset(offset + 10);
-	}
+		setOffset(off+10);
+	};
 
 	// can add a search bar to look for specific cards
 	// TODO: could add feature to not display pokemon that are on the team
 	return (
 		<div className='dex'>
 			{/* TODO: check for null or network error and display smth different */}
-			
 			<div className='cards'>{pokemonCards}</div>
-			<div className='button load-button' onClick={handleLoadMore}>Load More...</div>
+			<div className='bottom-buttons'>
+				<div className='button load-button' onClick={handleLoadMore}>Load More...</div>
+				<div className='button load-button randomize-button' onClick={randomizeDex}>?Randomize Dex?</div>
+			</div>
+			<div>Tooltips:</div>
+			<div>Refresh the page to go back to the beginning of the dex</div>
+			<div>You can have a max of 6 Pokemon on your team</div>
 		</div>
 	);
 
